8f9b01ff547b3ec57429c0b96aef99657dbeb1b9
Korubi <110281824+K0ruru@users.noreply.github.com>
2024-02-27 19:24:06 +0700

    made signup logic

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,8 @@
 
 import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
 import NexText from "../../../../public/NexText.png";
 
 import "./styles.scss";
@@ -11,10 +12,34 @@ export default function Signup() {
 	const [show, setShow] = React.useState(false);
 	const handleClick = () => setShow(!show);
 
+	const Username = useRef<HTMLInputElement>(null);
+	const Email = useRef<HTMLInputElement>(null);
+	const Password = useRef<HTMLInputElement>(null);
+	const CPassword = useRef<HTMLInputElement>(null);
+
+	const signup = async () => {
+		try {
+			if (Password.current?.value !== CPassword.current?.value) {
+				console.log("Passwords do not match");
+				return;
+			}
+
+			const res = await axios.post("some/api", {
+				username: Username?.current?.value,
+				email: Email?.current?.value,
+				password: Password?.current?.value,
+			});
+
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
 		<>
 			<div className="container-center">
-				<form action="">
+				<form onSubmit={signup}>
 					<div className="form-header">
 						<Image src={NexText} alt="" className="nextext" />
 						<h1>Sign Up !</h1>
@@ -49,6 +74,16 @@ export default function Signup() {
 										{show ? "Hide" : "Show"}
 									</Button>
 								</InputRightElement>
+							</InputGroup>
+						</div>
+						<div className="input">
+							<InputGroup size="md">
+								<Input
+									variant=""
+									type={show ? "text" : "password"}
+									placeholder="Confrim Password"
+									className="my-input"
+								/>
 							</InputGroup>
 						</div>
 					</div>
